21a5c37635a53faebc039be04fc6175a1846b980
Matthew Dennis <matthew.dennis@rogers.com>
2022-12-06 11:56:35 -0500

    Start of getting GPU Info

---
+++ GpuInfo/Program.cs
@@ -0,0 +1,14 @@
+﻿// Copyright © Amer Koleci and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+using Vortice.GPU;
+
+
+var gpuDevice = GPUDevice.CreateDefault();
+
+Console.WriteLine("==== GPU Properties ====");
+Console.WriteLine($"Name: {gpuDevice.AdapterProperties.Name}");
+Console.WriteLine($"Description: {gpuDevice.AdapterProperties.DriverDescription}");
+Console.WriteLine($"VendorId: {gpuDevice.AdapterProperties.VendorId}");
+Console.WriteLine($"DeviceId: {gpuDevice.AdapterProperties.DeviceId}");
+Console.WriteLine($"AdapterType: {gpuDevice.AdapterProperties.AdapterType}");
